6c8701f084f1c422361636b84d88e98ef21d83ac
Marvin Garcia <magar@microsoft.com>
2019-08-05 14:01:14 -0500

    removed GET method from function

--- IoTHubFunctions/IoTHubFunctions/FormatEmail.cs
+++ IoTHubFunctions/IoTHubFunctions/FormatEmail.cs
@@ -28,7 +28,7 @@ namespace IoTHubFunctions
 
         [FunctionName("FormatEmail")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             try
